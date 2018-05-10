2154dd559ae571fc1012550c3cb13d03c6ae1ed9
MYSTOGAN\Admin <vietpv94@gmail.com>
2018-05-10 08:44:06 +0700

    first commit

--- buildExe.js
+++ buildExe.js
@@ -1,10 +1,11 @@
 var electronInstaller = require('electron-winstaller');
 
 resultPromise = electronInstaller.createWindowsInstaller({
-    appDirectory: './release/package/electron-form-demo-win32-x64',
+    appDirectory: './builds/electronformdemo-win32-x64',
     outputDirectory: './dist/installer',
     authors: 'Viet Pham',
-    exe: 'electron-form-demo.exe'
+    exe: 'electronformdemo.exe',
+    setupMsi: 'electrondemo.msi'
   });
 
 resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
--- main.js
+++ main.js
@@ -1,14 +1,93 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
+const appVersion = require('./package.json').version;
+const os = require('os').platform();
 
 let win
 const electron = require('electron');
-const squirrelUrl = "http://localhost:3333";
+const squirrelUrl = "'http://localhost:3000/updates/latest'";
 
+if (require('electron-squirrel-startup')) return;
+if (handleSquirrelEvent()) {
+    // squirrel event handled and app will exit in 1000ms, so don't do anything else
+    return;
+}
+
+//Other code
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+function handleSquirrelEvent() {
+  if (process.argv.length === 1) {
+    return false;
+  }
+
+  const ChildProcess = require('child_process');
+  const path = require('path');
+
+  const appFolder = path.resolve(process.execPath, '..');
+  const rootAtomFolder = path.resolve(appFolder, '..');
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+  const exeName = path.basename(process.execPath);
+
+  const spawn = function(command, args) {
+    let spawnedProcess, error;
+
+    try {
+      spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
+    } catch (error) {}
+
+    return spawnedProcess;
+  };
+
+  const spawnUpdate = function(args) {
+    return spawn(updateDotExe, args);
+  };
+
+  const squirrelEvent = process.argv[1];
+  switch (squirrelEvent) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+      // Optionally do things such as:
+      // - Add your .exe to the PATH
+      // - Write to the registry for things like file associations and
+      //   explorer context menus
+
+      // Install desktop and start menu shortcuts
+      spawnUpdate(['--createShortcut', exeName]);
+
+      setTimeout(electron.quit, 1000);
+      return true;
+
+    case '--squirrel-uninstall':
+      // Undo anything you did in the --squirrel-install and
+      // --squirrel-updated handlers
+
+      // Remove desktop and start menu shortcuts
+      spawnUpdate(['--removeShortcut', exeName]);
+
+      setTimeout(electron.quit, 1000);
+      return true;
+
+    case '--squirrel-obsolete':
+      // This is called on the outgoing version of your app before
+      // we update to the new version - it's the opposite of
+      // --squirrel-updated
+
+      app.quit();
+      return true;
+  }
+};
 const startAutoUpdater = (squirrelUrl) => {
 // The Squirrel application will watch the provided URL
-electron.autoUpdater.setFeedURL(`${squirrelUrl}/win64/`);
+if (process.env.NODE_ENV !== 'development') {
+    squirrelUrl = os === 'darwin' ?
+      'https://macOSserver/updates/latest' : // just a placeholder
+      'http://WindowsServer/releases/win32'; // just a placeholder
+  }
+  
+electron.autoUpdater.setFeedURL(squirrelUrl + '?v=' + appVersion);
 
 // Display a success message on successful update
 electron.autoUpdater.addListener("update-downloaded", (event, releaseNotes, releaseName) => {
@@ -52,27 +131,3 @@ app.on('activate', () => {
     createWindow()
     }
 })
-
-const handleSquirrelEvent = () => {
-    if (process.argv.length === 1) {
-        return false;
-    }
-    
-    const squirrelEvent = process.argv[1];
-    switch (squirrelEvent) {
-        case '--squirrel-install':
-        case '--squirrel-updated':
-        case '--squirrel-uninstall':
-        setTimeout(app.quit, 1000);
-        return true;
-    
-        case '--squirrel-obsolete':
-        app.quit();
-        return true;
-    }
-}
-    
-if (handleSquirrelEvent()) {
-    // squirrel event handled and app will exit in 1000ms, so don't do anything else
-    return;
-}
