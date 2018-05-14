e4baa4e9dcd05dbb43867aa6848d806eea8ca80f
IBCT <ibct@IBCTs-MacBook-Pro.local>
2018-05-14 16:24:32 +0700

    osx installer added

--- buildOSX.js
+++ buildOSX.js
@@ -1,7 +1,7 @@
 const createDMG = require('electron-installer-dmg')
 
 const opts = {
-    "appPath" : "./builds/electron-form-demo-darwin-x64",
+    "appPath" : "./builds/electronformdemo-darwin-x64",
     "name" : "electron-form-demo"
 }
 createDMG(opts, function done (err) { })
